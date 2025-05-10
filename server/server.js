48d327fcd1d4e97dbd0e8766f2f4a422ff27cb37
Aman kumar <amankumar105541@gmail.com>
2025-05-10 23:53:31 +0530

    Connext database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,24 @@
 import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+
+
+dotenv.config();
+connectDB();
+
 
 const app=express();
 
-const port=5000
+const PORT= process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
 
 app.get('/',(req,res)=>{
     res.send("API is running...")
 })
 
-app.listen(port,()=>{console.log(`Server is running at ${port}`)})
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

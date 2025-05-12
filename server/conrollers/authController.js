5a6a0422b2753387d9ec6e06c79a9777b145d073
Aman kumar <amankumar105541@gmail.com>
2025-05-12 23:28:23 +0530

    Generate jwt token

---
+++ server/conrollers/authController.js
@@ -0,0 +1,9 @@
+import User from './models/User.js';
+import jwt from 'jsonwebtoken';
+
+// Generate JWT token
+const generateToken = (userId) => {
+  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};

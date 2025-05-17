be58627b654f0b957b35fde35949ccf25eddb2ff
Aman kumar <amankumar105541@gmail.com>
2025-05-17 22:24:42 +0530

    Notification model

---
+++ server/models/Notification.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+  recipient: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    default: null, // Optional: link to capsule or request page
+  },
+  isRead: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Notification = mongoose.model("Notification", notificationSchema);
+export default Notification;

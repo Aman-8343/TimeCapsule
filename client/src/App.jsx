537587f75f5b070e37f286f4ef3d38a42467c12e
Aman kumar <amankumar105541@gmail.com>
2025-05-22 18:56:34 +0530

    fixing error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/create" element={<CreateCapsule />} />

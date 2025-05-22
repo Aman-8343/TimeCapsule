25a512746bb0312099a829b2148a522e1d09c5da
Aman kumar <amankumar105541@gmail.com>
2025-05-22 10:55:12 +0530

    setup all routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from 'react'
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CreateCapsule from "./pages/CreateCapsule";
+import ViewCapsule from "./pages/ViewCapsule";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     gfdgfdg
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/create" element={<CreateCapsule />} />
+        <Route path="/capsule/:id" element={<ViewCapsule />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;

ab9d29094efbe6ae37f4d11ff420100d753c68df
Aman kumar <amankumar105541@gmail.com>
2025-05-22 10:16:44 +0530

    Adding routes of capsule

--- server/routes/capsuleRoutes.js
+++ server/routes/capsuleRoutes.js
@@ -1,13 +1,17 @@
 
 import express from "express";
-import { createCapsule } from "../controllers/capsuleController.js";
+import { createCapsule,
+    getMyCapsules,
+    updateCapsule,
+    deleteCapsule
+ } from "../controllers/capsuleController.js";
 import protect from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// @route   POST /api/capsules
-// @desc    Create a new capsule
-// @access  Private
 router.post("/", protect, createCapsule);
+router.get("/", protect, getMyCapsules);
+router.put("/:id", protect, updateCapsule);
+router.delete("/:id", protect, deleteCapsule);
 
 export default router;

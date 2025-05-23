7f680d26170ddca6617c84916964f44cec38a8b8
Aman kumar <amankumar105541@gmail.com>
2025-05-24 00:45:40 +0530

    created unlock capsule

--- server/routes/capsuleRoutes.js
+++ server/routes/capsuleRoutes.js
@@ -1,21 +1,26 @@
 
 import express from "express";
+import upload from "../middlewares/uploadMiddleware.js";
+
 import { createCapsule,
     getMyCapsules,
     updateCapsule,
     deleteCapsule,
-    getCapsuleById
+    getCapsuleById,
+    unlockCapsule
  } from "../controllers/capsuleController.js";
 import protect from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", protect, createCapsule);
+router.post("/", protect, upload.single("file"), createCapsule);
 router.get("/", protect, getMyCapsules);
 router.put("/:id", protect, updateCapsule);
 router.delete("/:id", protect, deleteCapsule);
 router.get("/:id", protect, getCapsuleById);
+router.get("/unlock/:id", authMiddleware, unlockCapsule);
 
-router.post("/", protect, upload.single("file"), createCapsule);
+
+
 
 export default router;

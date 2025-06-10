625bb6a8e10161266b612e31d8e6c773ec51f03c
Aman kumar <amankumar105541@gmail.com>
2025-06-10 09:05:38 +0530

    update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { loginUser } from "../services/authService";
+import { loginUser } from "./services/authService";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {

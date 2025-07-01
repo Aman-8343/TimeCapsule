2fdd9c2135eef9a1b842bcbf500582156a92d8b6
Aman kumar <amankumar105541@gmail.com>
2025-07-02 00:06:02 +0530

    fixing storage problem

--- server/middlewares/uploadMiddleware.js
+++ server/middlewares/uploadMiddleware.js
@@ -1,7 +1,6 @@
 import multer from "multer";
-import { storage } from "../utils/cloudinary.js"; 
+import { storage } from "../utils/cloudinary.js";
 
-const memoryStorage = multer.memoryStorage(); 
-const upload = multer({ storage: memoryStorage });
+const upload = multer({ storage });
 
 export default upload;
